29f665be6f6e583ba1eab096ef0b858dc8ab9d62
bhargavbhegde7@gmail.com <bhargavbhegde7@gmail.com>
2019-05-11 19:03:06 +0000

    changing the python code

--- push_utils.py
+++ push_utils.py
@@ -1,13 +1,11 @@
-#from git import Repo
-#import subprocess
+from git import Repo
 
-'''
 print("getting the repo etc")
 
 repo_dir = '/home/pi/bhargavbhegde7.github.io'
 repo = Repo(repo_dir)
 file_list = ['utils.html']
-commit_message = 'Change ip address of pi'
+commit_message = 'Change the tunnel url'
 repo.index.add(file_list)
 print("files added")
 repo.index.commit(commit_message)
@@ -18,23 +16,17 @@ origin.push()
 print("pushed")
 
 print("done deal!!")
-'''
 
+
+'''
 import subprocess
 import os
 os.chdir("/home/pi/bhargavbhegde7.github.io/")
-#subprocess.call(["cd", "/home/pi/bhargavbhegde7.github.io/"], shell=True)
-#subprocess.call(["ls", "-ltrh"], shell=True)
-#subprocess.call(["df", "-h"])
-
 subprocess.call(["git config credential.helper store"], shell=True)
-#subprocess.call(["git status"], shell=True)
 subprocess.call(["git add ."], shell=True)
 print("added")
-#subprocess.call(["git add ."], shell=True)
-#subprocess.call(["git status"], shell=True)
 subprocess.call(["git commit -m \"tunnel url change\" "], shell=True)
 print("committed")
 subprocess.call(["git push origin master"], shell=True)
 print("pushed")
-
+'''
